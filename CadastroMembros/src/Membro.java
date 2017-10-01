1b56d769e3a691b2d0686069f88a8ce0819f8ba7
LucasPLTC <lucas.carvalho.pltc@gmail.com>
2017-10-01 14:53:23 -0300

    Classe membro

--- CadastroMembros/src/Membro.java
+++ CadastroMembros/src/Membro.java
@@ -1,4 +1,69 @@
 
 public class Membro {
+	 
+      private String nome;
+      private String email;
+      private String curso;
+      private String tel;
+      private String equipe;
+      private String bairro;
+      private boolean Pagante;
+      private boolean ativo;
+      
+      public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEquipe() {
+		return equipe;
+	}
+	public void setEquipe(String equipe) {
+		this.equipe = equipe;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public boolean isPagante() {
+		return Pagante;
+	}
+	public void setPagante(boolean pagante) {
+		Pagante = pagante;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+      
+      
+      
+      
+     
+	
 
 }
