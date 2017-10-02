b2809c4a238865e2d2fd06a7bb8131c484c8bb76
LucasPLTC <lucas.carvalho.pltc@gmail.com>
2017-10-02 00:30:33 -0300

    d

--- CadastroMembros/src/interfacemenu/Menuprincipal.java
+++ CadastroMembros/src/interfacemenu/Menuprincipal.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JDesktopPane;
+import javax.swing.JToolBar;
+import javax.swing.UIManager;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
 
 public class Menuprincipal extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -31,12 +40,23 @@ public class Menuprincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public Menuprincipal() {
+		setFont(new Font("Arial Black", Font.PLAIN, 14));
+		setForeground(UIManager.getColor("CheckBoxMenuItem.acceleratorForeground"));
+		setEnabled(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 500, 500);
+		getContentPane().setLayout(null);
+		
+		JFormattedTextField frmtdtxtfldControleDeMembros = new JFormattedTextField();
+		frmtdtxtfldControleDeMembros.setHorizontalAlignment(SwingConstants.CENTER);
+		frmtdtxtfldControleDeMembros.setForeground(SystemColor.desktop);
+		frmtdtxtfldControleDeMembros.setFont(new Font("Arial Black", Font.PLAIN, 14));
+		frmtdtxtfldControleDeMembros.setText("Controle de membros Ramo IEEE CEFET-RJ");
+		frmtdtxtfldControleDeMembros.setBounds(89, 12, 348, 37);
+		getContentPane().add(frmtdtxtfldControleDeMembros);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(142, 165, 6, 22);
+		getContentPane().add(textPane);
 	}
-
 }
